27d1ef7f440fa9ea0339ca80bc1513925e7224b2
Debojit323350 <debojitkumarbarman@gmail.com>
2024-10-15 03:08:28 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import Resturent from './component/Basics/Resturent';
+import Temp from './component/weather/Temp'
+ 
+
+const App = () => {
+  return <Temp/>
+};
+
+export default App;
+
